806e2a8399a67e48e7295ae0ae37de17c4432ca4
Michael Ibitoye <michael303.mi@gmail.com>
2022-09-26 18:01:32 +0100

    removed long file

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Container,StyledFooter} from './styles/SignInPage'
 import SignInForm from 'features/signIn/forms/SignInForm'
 const footerItems=require('data/signInPageFooter.json')
-const image=require('assets/images/misc/NetflixAssets/HeaderPicture.jpg')
+const image=require('assets/images/misc/HeaderPicture.jpg')
 
 const SignInPage = () => {
     return (
