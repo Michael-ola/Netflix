40dcb2a0ce8a072901aab33927bb68487807fcbf
michael-ola <michael303.mi@gmail.com>
2024-06-14 13:41:22 +0100

    fixing google flag

--- src/features/payment/forms/PaymentForm/index.tsx
+++ src/features/payment/forms/PaymentForm/index.tsx
@@ -1,130 +1,153 @@
-import React,{useCallback, useState} from 'react'
-import {Form,CardIcon,FormHeader,Step,AgreementContainer} from './style/PaymentForm.style'
-import {Link,useNavigate} from 'react-router-dom'
+import React, { useCallback, useState } from "react";
+import {
+  Form,
+  CardIcon,
+  FormHeader,
+  Step,
+  AgreementContainer,
+} from "./style/PaymentForm.style";
+import { Link, useNavigate } from "react-router-dom";
 //import Recaptcha from 'react-google-recaptcha'
-import {useSelector} from 'react-redux'
-import SubmitButton from 'components/NextPrevButton'
-import FormContextProvider from 'context/formsContext'
-import FirstName from './firstName'
-import LastName from './lastName'
-import CardNumber from './cardNumber'
-import SecurityCode from './securityCode'
-import ExpirationDate from './expirationDate'
-import PlanInfo from './PlanInfo'
-import CheckBox from './checkBox'
-import {setUser} from 'services/firebase/setUsersData'
-import userType from 'types/usersDataType'
-import { stateType } from 'lib/redux-store/types'
-import {v4 as uuidv4} from 'uuid'
+import { useSelector } from "react-redux";
+import SubmitButton from "components/NextPrevButton";
+import FormContextProvider from "context/formsContext";
+import FirstName from "./firstName";
+import LastName from "./lastName";
+import CardNumber from "./cardNumber";
+import SecurityCode from "./securityCode";
+import ExpirationDate from "./expirationDate";
+import PlanInfo from "./PlanInfo";
+import CheckBox from "./checkBox";
+import { setUser } from "services/firebase/setUsersData";
+import userType from "types/usersDataType";
+import { stateType } from "lib/redux-store/types";
+import { v4 as uuidv4 } from "uuid";
 
-const visaImg=require('assets/images/icons/visa-v3.png')
-const mastercardImg=require('assets/images/icons/mastercard-v2.png')
-const verveImg=require('assets/images/icons/icon_verve.png')
+const visaImg = require("assets/images/icons/visa-v3.png");
+const mastercardImg = require("assets/images/icons/mastercard-v2.png");
+const verveImg = require("assets/images/icons/icon_verve.png");
 
 const PaymentForm = () => {
-    const [submitButtonClicked,setSubmitButtonClicked]=useState(false)
-    const navigate=useNavigate()
-    const userId=useSelector((state:stateType)=>{
-        return state.auth.userId;
-    }) as string
+  const [submitButtonClicked, setSubmitButtonClicked] = useState(false);
+  const navigate = useNavigate();
+  const userId = useSelector((state: stateType) => {
+    return state.auth.userId;
+  }) as string;
 
-    const email=useSelector((state:stateType)=>{
-        return state.emailStore.email;
-    })
+  const email = useSelector((state: stateType) => {
+    return state.emailStore.email;
+  });
 
-    const planInfo=useSelector((state:stateType)=>{
-        return state.planInfo
-    })
+  const planInfo = useSelector((state: stateType) => {
+    return state.planInfo;
+  });
 
-    const [formObject,setFormObject]=useState(
-        {
-            firstName:'',
-            lastName:'',
-            cardNumber:'',
-            expirationDate:'',
-            securityCode:'',
-            checkBox:false,
-        }
-    )
+  const [formObject, setFormObject] = useState({
+    firstName: "",
+    lastName: "",
+    cardNumber: "",
+    expirationDate: "",
+    securityCode: "",
+    checkBox: false,
+  });
 
-    
-    const setFormData=useCallback((inputName:string,value:boolean|boolean) => {
-        setFormObject(prevData=>{
-            const formCopy={...prevData,[inputName]:value}
-            return formCopy
-        })
-    },[])
+  const setFormData = useCallback(
+    (inputName: string, value: boolean | boolean) => {
+      setFormObject((prevData) => {
+        const formCopy = { ...prevData, [inputName]: value };
+        return formCopy;
+      });
+    },
+    []
+  );
 
-    const buttonClickedHandler=(event:React.MouseEvent<HTMLButtonElement>)=>{
-        setSubmitButtonClicked(true);
+  const buttonClickedHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setSubmitButtonClicked(true);
+  };
+  const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const emptyFields = Object.entries(formObject).filter((input) => {
+      return !input[1];
+    });
 
+    if (
+      (emptyFields?.[0]?.[0] === "lastName" && emptyFields.length === 1) ||
+      emptyFields.length === 0
+    ) {
+      const user: userType = {
+        id: uuidv4() as string,
+        permissions: "owner",
+        username: `${formObject.firstName.trim()} ${formObject.lastName.trim()}`,
+        "email-address": email,
+        "phone-number": "",
+        avatar: "orangeAvatar.png",
+        "movies-list": [],
+        "subscription-status": "active",
+        "subscription-type": planInfo.planName,
+      };
+      setUser(user, userId, () => navigate("/select-user"));
     }
-    const formSubmitHandler=(event:React.FormEvent<HTMLFormElement>)=>{
-        event.preventDefault();
-        const emptyFields=Object.entries(formObject).filter(input=>{
-            return !input[1]
-        });
+  };
+  return (
+    <FormContextProvider
+      setFormData={setFormData}
+      submitButtonClicked={submitButtonClicked}
+    >
+      <FormHeading />
+      <Form method="POST" autoComplete="off" onSubmit={formSubmitHandler}>
+        <FirstName />
+        <LastName />
+        <CardNumber />
+        <ExpirationDate />
+        <SecurityCode />
+        <PlanInfo />
+        <Agreement />
+        <CheckBox />
+        <SubmitButton
+          onClick={buttonClickedHandler}
+          spinner={submitButtonClicked}
+        >
+          Start Membership
+        </SubmitButton>
+      </Form>
+    </FormContextProvider>
+  );
+};
 
-        if((emptyFields?.[0]?.[0]==='lastName' && emptyFields.length===1) || emptyFields.length===0){
-            const user:userType={
-                id:uuidv4() as string,
-                permissions:"owner",
-                username:`${formObject.firstName.trim()} ${formObject.lastName.trim()}`,
-                "email-address":email,
-                "phone-number": "",
-                avatar:"orangeAvatar.png",
-                "movies-list":[],
-                "subscription-status":"active",
-                "subscription-type":planInfo.planName
-            };
-            setUser(user,userId,()=>navigate('/select-user'))
-        }
-    }
-    return (
-        <FormContextProvider setFormData={setFormData} submitButtonClicked={submitButtonClicked}>
-            <FormHeading/>
-            <Form autoComplete='off' onSubmit={formSubmitHandler}>
-                <FirstName/>
-                <LastName/>
-                <CardNumber/>
-                <ExpirationDate/>
-                <SecurityCode/>
-                <PlanInfo/>
-                <Agreement/>
-                <CheckBox/>
-                <SubmitButton onClick={buttonClickedHandler} spinner={submitButtonClicked}>Start Membership</SubmitButton>
-            </Form>
-        </FormContextProvider>
-    )
-}
+const FormHeading = () => (
+  <>
+    <Step>
+      STEP <strong>3</strong> OF <strong>3</strong>
+    </Step>
+    <FormHeader>Set up your credit or debit card</FormHeader>
+    <div>
+      <CardIcon src={visaImg} />
+      <CardIcon src={mastercardImg} />
+      <CardIcon src={verveImg} />
+    </div>
+  </>
+);
 
-const  FormHeading=()=>(
-    <>
-        <Step>STEP <strong>3</strong> OF <strong>3</strong></Step>
-        <FormHeader>Set up your credit or debit card</FormHeader>
-        <div>
-            <CardIcon src={visaImg}/>
-            <CardIcon src={mastercardImg}/>
-            <CardIcon src={verveImg}/>
-        </div>
-    </>
-)
-
-const Agreement=()=>{
-    const planInfo=useSelector((state:stateType)=>{
-        return state.planInfo
-    })
-    return(
+const Agreement = () => {
+  const planInfo = useSelector((state: stateType) => {
+    return state.planInfo;
+  });
+  return (
     <AgreementContainer>
-        <p>Your payments will be processed internationally. Additional bank fees may apply</p>
-        <p>By checking the checkbox below, you agree to our <Link to="">Terms of Use</Link>,
-           <Link to="">Privacy Statement</Link>,
-           and that you are over 18. Netflix will automatically continue your membership and
-           charge the membership fee{` (currently ${planInfo.planPrice}/month) `}to your payment method until you
-           cancel. You may cancel at any time to avoid future charges.
-        </p>
+      <p>
+        Your payments will be processed internationally. Additional bank fees
+        may apply
+      </p>
+      <p>
+        By checking the checkbox below, you agree to our{" "}
+        <Link to="">Terms of Use</Link>,<Link to="">Privacy Statement</Link>,
+        and that you are over 18. Netflix will automatically continue your
+        membership and charge the membership fee
+        {` (currently ${planInfo.planPrice}/month) `}to your payment method
+        until you cancel. You may cancel at any time to avoid future charges.
+      </p>
     </AgreementContainer>
-)}
+  );
+};
 
-
-export default PaymentForm
+export default PaymentForm;
--- src/features/signIn/forms/SignInForm/index.tsx
+++ src/features/signIn/forms/SignInForm/index.tsx
@@ -1,139 +1,228 @@
-import React,{useState} from 'react'
-import { useForm, SubmitHandler, Control, UseFormGetValues, UseFormRegister, FieldError } from "react-hook-form";
-import {Container,Title,StyledForm,CheckBoxContainer,
-        CheckBox,CheckboxAndHelpContainer,Help,SignUpLink,
-        MetaTextContainer,LearnMoreLink} from './style/SignInForm'
-import  useInput from 'customHooks/useInput/useInput'
-import {useDispatch,useSelector} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
-import {auth} from 'lib/redux-store/Actions/auth'
-import SubmitButton from 'components/NextPrevButton'
-import {stateType} from 'lib/redux-store/types'
-import { AnyAction } from 'redux';
-import withErrorIndicator from 'HOC/withErrorIndicator'
-import axios  from 'axios'
+import React, { useState } from "react";
+import {
+  useForm,
+  SubmitHandler,
+  Control,
+  UseFormGetValues,
+  UseFormRegister,
+  FieldError,
+} from "react-hook-form";
+import {
+  Container,
+  Title,
+  StyledForm,
+  CheckBoxContainer,
+  CheckBox,
+  CheckboxAndHelpContainer,
+  Help,
+  SignUpLink,
+  MetaTextContainer,
+  LearnMoreLink,
+} from "./style/SignInForm";
+import useInput from "customHooks/useInput/useInput";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { auth } from "lib/redux-store/Actions/auth";
+import SubmitButton from "components/NextPrevButton";
+import { stateType } from "lib/redux-store/types";
+import { AnyAction } from "redux";
+import withErrorIndicator from "HOC/withErrorIndicator";
+import axios from "axios";
 
 interface IFormInput {
-    signInContact: string,
-    password: string
+  signInContact: string;
+  password: string;
 }
 
-interface inputType{
-    errors: {
-        signInContact?: FieldError | undefined;
-        password?: FieldError | undefined;
-    },
-    register: UseFormRegister<IFormInput>,
-    getValues: UseFormGetValues<IFormInput>,
-    submitButtonClicked: boolean | null,
-    control?:Control<IFormInput, any>
+interface inputType {
+  errors: {
+    signInContact?: FieldError | undefined;
+    password?: FieldError | undefined;
+  };
+  register: UseFormRegister<IFormInput>;
+  getValues: UseFormGetValues<IFormInput>;
+  submitButtonClicked: boolean | null;
+  control?: Control<IFormInput, any>;
 }
 
-
-function SignInForm(){
-    return (
-        <Container>
-            <Title>Sign In</Title>
-            <Form/>
-        </Container>
-    )
+function SignInForm() {
+  return (
+    <Container>
+      <Title>Sign In</Title>
+      <Form />
+    </Container>
+  );
 }
 
-const Form=()=>{
-    const [submitButtonClicked,setSubmitButtonClicked]=useState<boolean|null>(null)
-    const { register,control,formState: { errors }, handleSubmit,getValues } = useForm<IFormInput>();
-    const navigate=useNavigate();
-    const dispatch = useDispatch();
+const Form = () => {
+  const [submitButtonClicked, setSubmitButtonClicked] = useState<
+    boolean | null
+  >(null);
+  const {
+    register,
+    control,
+    formState: { errors },
+    handleSubmit,
+    getValues,
+  } = useForm<IFormInput>();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const loadingState=useSelector((state:stateType)=>{
-        return state.auth.loading
-    })
+  const loadingState = useSelector((state: stateType) => {
+    return state.auth.loading;
+  });
 
-    // const token=useSelector((state:stateType)=>{
-    //     return state.auth.token
-    // })
+  // const token=useSelector((state:stateType)=>{
+  //     return state.auth.token
+  // })
 
-    // useEffect(()=>{
-    //     submitButtonClicked && token && 
-    // },[navigate, submitButtonClicked, token])
-    
-    const buttonClickedHandler=()=>{
-      setSubmitButtonClicked(true)
+  // useEffect(()=>{
+  //     submitButtonClicked && token &&
+  // },[navigate, submitButtonClicked, token])
+
+  const buttonClickedHandler = () => {
+    setSubmitButtonClicked(true);
+  };
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    const contact = data.signInContact.trim();
+    const password = data.password.trim();
+    if (contact && password) {
+      dispatch(
+        auth(contact, password, false, undefined, () =>
+          navigate("/select-user")
+        ) as unknown as AnyAction
+      );
     }
-    const onSubmit: SubmitHandler<IFormInput> = (data) =>{
-        const contact=data.signInContact.trim();
-        const password=data.password.trim();
-        if(contact && password){
-            dispatch(auth(contact, password, false,undefined,()=>navigate('/select-user')) as unknown as AnyAction);
-        }
-    }
-    
-    return(
-        <StyledForm autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
-            <ContactInput control={control} errors={errors} register={register} getValues={getValues} 
-             submitButtonClicked={submitButtonClicked}/>
-            
-            <PasswordInput errors={errors} register={register} getValues={getValues}
-             submitButtonClicked={submitButtonClicked}/>
+  };
 
-            <SubmitButton spinner={loadingState} onClick={buttonClickedHandler}>Sign In</SubmitButton>
+  return (
+    <StyledForm
+      method="POST"
+      autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <ContactInput
+        control={control}
+        errors={errors}
+        register={register}
+        getValues={getValues}
+        submitButtonClicked={submitButtonClicked}
+      />
 
-            <CheckboxAndHelp/>
-            <MetaText/>
-        </StyledForm>
-    )
-}
+      <PasswordInput
+        errors={errors}
+        register={register}
+        getValues={getValues}
+        submitButtonClicked={submitButtonClicked}
+      />
 
-const ContactInput=({errors,register,getValues,submitButtonClicked,control}:inputType)=>{
-    const userContactInput= useInput({errors, getValues, register, 
-        submitButtonState:submitButtonClicked,type:'Email/Telephone',name:'signInContact',control,
-        placeholder:'Email or Phone number',required:'Please enter a valid email or phone number'
-    })
-    return(
-        <>{
-            userContactInput({height:'60px',width:'100%',color:'#fff',border:'none',
-            background:'#333333',switchBackground:'#454545',borderRadius:'4px',
-            labelColor:'#8c8c8c',errorFontSize:'14px',padding:'13px 0 0 0'})
-        }</>
-    )
-}
+      <SubmitButton spinner={loadingState} onClick={buttonClickedHandler}>
+        Sign In
+      </SubmitButton>
 
-const PasswordInput =({errors,register,getValues,submitButtonClicked}:inputType)=>{
-    const message='Your password must contain between 4 and 60 characters.'
-    const passwordInput= useInput({errors, getValues, register, 
-        submitButtonState:submitButtonClicked,type:'password',name:'password',placeholder:'Password',
-        required:message,minLength:[4,message],
-        maxLength:[60,message]
-    })
-    return(
-        <>{
-            passwordInput({height:'60px',width:'100%',color:'#fff',border:'none',
-            background:'#333333',switchBackground:'#454545',borderRadius:'4px',
-            labelColor:'#8c8c8c',errorFontSize:'14px',padding:'13px 0 0 0'})
-        }</>
-    )
-}
+      <CheckboxAndHelp />
+      <MetaText />
+    </StyledForm>
+  );
+};
 
-const CheckboxAndHelp=()=>{
-    return(
-        <CheckboxAndHelpContainer>
-            <CheckBoxContainer>
-                <CheckBox id='checkbox'  type='checkbox' />
-                <label htmlFor='checkbox'>Remember me</label>
-            </CheckBoxContainer>
-            <Help>Need help?</Help>
-        </CheckboxAndHelpContainer>
-    )
-}
+const ContactInput = ({
+  errors,
+  register,
+  getValues,
+  submitButtonClicked,
+  control,
+}: inputType) => {
+  const userContactInput = useInput({
+    errors,
+    getValues,
+    register,
+    submitButtonState: submitButtonClicked,
+    type: "Email/Telephone",
+    name: "signInContact",
+    control,
+    placeholder: "Email or Phone number",
+    required: "Please enter a valid email or phone number",
+  });
+  return (
+    <>
+      {userContactInput({
+        height: "60px",
+        width: "100%",
+        color: "#fff",
+        border: "none",
+        background: "#333333",
+        switchBackground: "#454545",
+        borderRadius: "4px",
+        labelColor: "#8c8c8c",
+        errorFontSize: "14px",
+        padding: "13px 0 0 0",
+      })}
+    </>
+  );
+};
 
-const MetaText=()=>{
-    return(
-        <MetaTextContainer>
-            <h3>New to Netflix? <SignUpLink to='/'>Sign UP now.</SignUpLink></h3>
-            <h5>{`This page is protected by Google reCAPTCHA to ensure you're not a bot. `}
-                <LearnMoreLink to='/'>Learn more</LearnMoreLink></h5>
-        </MetaTextContainer>
-    )
-}
+const PasswordInput = ({
+  errors,
+  register,
+  getValues,
+  submitButtonClicked,
+}: inputType) => {
+  const message = "Your password must contain between 4 and 60 characters.";
+  const passwordInput = useInput({
+    errors,
+    getValues,
+    register,
+    submitButtonState: submitButtonClicked,
+    type: "password",
+    name: "password",
+    placeholder: "Password",
+    required: message,
+    minLength: [4, message],
+    maxLength: [60, message],
+  });
+  return (
+    <>
+      {passwordInput({
+        height: "60px",
+        width: "100%",
+        color: "#fff",
+        border: "none",
+        background: "#333333",
+        switchBackground: "#454545",
+        borderRadius: "4px",
+        labelColor: "#8c8c8c",
+        errorFontSize: "14px",
+        padding: "13px 0 0 0",
+      })}
+    </>
+  );
+};
 
-export default withErrorIndicator(SignInForm,axios)
+const CheckboxAndHelp = () => {
+  return (
+    <CheckboxAndHelpContainer>
+      <CheckBoxContainer>
+        <CheckBox id="checkbox" type="checkbox" />
+        <label htmlFor="checkbox">Remember me</label>
+      </CheckBoxContainer>
+      <Help>Need help?</Help>
+    </CheckboxAndHelpContainer>
+  );
+};
+
+const MetaText = () => {
+  return (
+    <MetaTextContainer>
+      <h3>
+        New to Netflix? <SignUpLink to="/">Sign UP now.</SignUpLink>
+      </h3>
+      <h5>
+        {`This page is protected by Google reCAPTCHA to ensure you're not a bot. `}
+        <LearnMoreLink to="/">Learn more</LearnMoreLink>
+      </h5>
+    </MetaTextContainer>
+  );
+};
+
+export default withErrorIndicator(SignInForm, axios);
